094fd5aeb5a24addf3b81641c2ebff7c9a027db9
Santoshjadhav1994 <santoshmjadhav1994@gmail.com>
2024-05-24 18:40:26 +0530

    nc

--- drfecommerce/drfecommerce/settings/base.py
+++ drfecommerce/drfecommerce/settings/base.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # External packages
     "rest_framework",
+    "drf_spectacular",
     # Internal Apps
+    "drfecommerce.product",
 ]
 
 MIDDLEWARE = [
@@ -115,4 +117,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Django DRF Ecommerce",
+}
---
+++ drfecommerce/drfecommerce/tests/conftest.py
@@ -0,0 +1,13 @@
+from pytest_factoryboy import register
+import pytest
+
+from .factories import CategoryFactory,BrandFactory,ProductFactory
+from rest_framework.test import APIClient
+
+register(CategoryFactory)
+register(BrandFactory)
+register(ProductFactory)
+
+@pytest.fixture
+def api_client():
+    return APIClient
