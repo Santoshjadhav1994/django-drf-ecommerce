0c2fa691afa807cb252779eb00929a7337a5db0a
Santoshjadhav1994 <santoshmjadhav1994@gmail.com>
2024-05-26 17:42:39 +0530

    nc

--- drfecommerce/drfecommerce/tests/product/test_models.py
+++ drfecommerce/drfecommerce/tests/product/test_models.py
@@ -4,46 +4,50 @@ from django.core.exceptions import ValidationError
 
 pytestmark = pytest.mark.django_db
 
+
 class TestCategoryModel:
-    def test_str_method(self,category_factory):
+    def test_str_method(self, category_factory):
         # Arrange
-        
+
         # Act
         x = category_factory(name="test_cat")
         # Assert
         assert x.__str__() == "test_cat"
-        
 
 
 class TestBrandModel:
-    def test_str_method(self,brand_factory):
+    def test_str_method(self, brand_factory):
         # Arrange
-        
+
         # Act
         x = brand_factory(name="test_brand")
         # Assert
         assert x.__str__() == "test_brand"
 
 
-
 class TestProductModel:
-    def test_str_method(self,product_factory):
+    def test_str_method(self, product_factory):
         # Arrange
-        
+
         # Act
         x = product_factory(name="test_product")
         # Assert
         assert x.__str__() == "test_product"
-        
+
 
 class TestProductLineModel:
-    def test_str_method(self,product_line_factory):
+    def test_str_method(self, product_line_factory):
         obj = product_line_factory(sku="12345")
         assert obj.__str__() == "12345"
-        
-    
-    def test_duplicate_order_values(self,product_line_factory,product_factory):
+
+    def test_duplicate_order_values(self, product_line_factory, product_factory):
         obj = product_factory()
-        product_line_factory(order=1,product=obj)
+        product_line_factory(order=1, product=obj)
         with pytest.raises(ValidationError):
-            product_line_factory(order=1,product=obj).clean()
+            product_line_factory(order=1, product=obj).clean()
+
+
+class TestProductImageModel:
+    def test_str_method(self, product_image_factory):
+        obj = product_image_factory(order=1)
+        assert obj.__str__() == "1"
